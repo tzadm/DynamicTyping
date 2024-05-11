d59a26ea6c796aae1569a5c1086613755ea94fe3
tzadm <94993624+tzadm@users.noreply.github.com>
2024-05-11 19:01:26 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+name = 'Timur'
+print('name:      ',name)
+age = 43
+print('age:       ', age)
+age = 43+1
+print('New age:   ',str(age))
+is_student = name
+print('Is Student:', is_student is name)
+
+
+
+
